fad43e7d82be3a78993d026a3ba2ae962d59ae39
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-17 00:24:07 -0700

    implemented update and edit

--- routes/rooms.js
+++ routes/rooms.js
@@ -6,38 +6,70 @@ const Room = require('../models/room');
 
 // Rooms index
 router.get('/', (req, res, next) => {
-  // TODO
+    Room.find({}, 'topic', function(err, rooms) {
+        if (err) {
+            console.error(err);
+        } else {
+            res.render('rooms/index', {
+                rooms: rooms
+            });
+        }
+    });
 });
 
 // Rooms new
 router.get('/new', auth.requireLogin, (req, res, next) => {
-  res.render('rooms/new');
+    res.render('rooms/new');
 });
 
 // Rooms show
 router.get('/:id', auth.requireLogin, (req, res, next) => {
-  // TODO
+    Room.findById(req.params.id, function(err, room) {
+        if (err) {
+            console.error(err)
+        };
+
+        res.render('rooms/show', {
+            room: room
+        });
+    });
 });
 
 // Rooms edit
 router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
-  // TODO
+    Room.findById(req.params.id, function(err, room) {
+        if (err) {
+            console.error(err)
+        };
+
+        res.render('rooms/edit', {
+            room: room
+        });
+    });
 });
 
 // Rooms update
 router.post('/:id', auth.requireLogin, (req, res, next) => {
-  // TODO
+
+    Room.findByIdAndUpdate(req.params.id, req.body, function(err, room) {
+        if (err) {
+            console.error(err)
+        };
+        res.redirect('/rooms/' + req.params.id);
+    });
 });
 
 // Rooms create
 router.post('/', auth.requireLogin, (req, res, next) => {
-  let room = new Room(req.body);
+    let room = new Room(req.body);
 
-  room.save(function(err, room) {
-    if(err) { console.error(err) };
+    room.save(function(err, room) {
+        if (err) {
+            console.error(err)
+        };
 
-    return res.redirect('/rooms');
-  });
+        return res.redirect('/rooms');
+    });
 });
 
 module.exports = router;
