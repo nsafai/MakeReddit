0dceee17963189549abef66fbbd81f91bbe08e7c
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-16 23:17:21 -0700

    adding routing for auth

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Make Reddit Baby' });
 });
 
+// login
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  console.log('logging in!');
+  console.log(req.body);
+
+  res.redirect('/');
+});
+
 module.exports = router;
